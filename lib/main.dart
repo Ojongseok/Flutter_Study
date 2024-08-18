6108f74521648970d18badb8dfa4d16c9b3ece6e
Ojongseok <whdtjr6889@naver.com>
2024-08-19 01:22:08 +0900

    Study13 AlertDialog, showDialog, context

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/study13.dart';
 import 'package:flutter_project/study14.dart';
 import 'package:flutter_project/study15.dart';
 import 'package:flutter_project/study16.dart';
@@ -7,7 +8,7 @@ import 'package:flutter_project/study16.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Study16(),
+      home: Study13(),
     )
   );
 }
--- lib/study13.dart
+++ lib/study13.dart
@@ -6,33 +6,49 @@ import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-class Study12 extends StatefulWidget {
-  const Study12({super.key});
+class Study13 extends StatefulWidget {
+  const Study13({super.key});
 
   @override
-  State<Study12> createState() => Study12State();
+  State<Study13> createState() => Study13State();
 }
 
-class Study12State extends State<Study12> {
+class Study13State extends State<Study13> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Test Title"),
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Image(
-            image: NetworkImage(
-              "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=13262118&filePath=L2Rpc2sxL25ld2RhdGEvMjAyMC8yMS9DTFMxMDAwNi82MmZhMWExMy03ZjRmLTQ1NWMtYTZlNy02ZTk2YjhjMjBkYTk=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10006"
-            ),
-            fit: BoxFit.fill,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Title"),
+      ),
+      body: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext con) {
+                    return AlertDialog(
+                      title: const Text("Dialog Title"),
+                      content: Container(
+                        child: const Text("Dialog Content"),
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("Close")
+                        )
+                      ],
+                    );
+                  }
+              );
+            },
+            child: const Text("Button"),
           ),
         ),
-      )
+      ),
     );
   }
 
